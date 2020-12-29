70b73781f06edae214ee440fa896103af81152d1
heiye1991 <1604043458@qq.com>
2020-12-29 11:21:35 +0800

    feat(styles-extract,package.json): extract CSS into separate files

---
+++ styles-extract/config/webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // 将CSS提取到单独的文件中
+const devMode = process.env.NODE_ENV === 'production' // 生产还是开发环境 生产使用MiniCssExtractPlugin，开发使用style-loader
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, '../src/index.js'),
+  output: {
+    publicPath: devMode ? '/' : './',
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      // 字体打包
+      {
+        test: /\.(woff|eot|ttf|otf)(\?.*)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts', // 设置打包后字体文件存放的文件夹名称
+              name: '[name]-[hash:5].[ext]' // 打包后的字体文件命名
+            }
+          }
+        ]
+      },
+      // css打包
+      {
+        test: /\.css$/,
+        use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      },
+      // less打包
+      {
+        test: /\.less$/,
+        use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'less-loader']
+      },
+      // sass/scss 打包
+      {
+        test: /\.(scss|sass)$/,
+        use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      // stylus打包
+      {
+        test: /\.styl$/,
+        use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'stylus-loader']
+      },
+      // 图片打包
+      {
+        test: /\.(png|jpg|jpeg|svg|gif)$/, // 正则匹配图片格式名 没有用到svg图片|svg去掉
+        use: [
+          // file-loader
+          /* {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images', // 设置打包后图片存放的文件夹名称
+              name: '[name]-[hash:5].[ext]' // 打包后的图片命名
+            }
+          } */
+          // url-loader 和 file-loader类似，但是文件大小（单位 byte）低于指定的限制时，可以返回一个 DataURL
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 20000, // 表示低于20000字节的图片会以base64编码
+              name: '[name]-[hash:5].[ext]', // 打包后的图片命名
+              outputPath: 'images' // 设置打包后图片存放的文件夹名称
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.css' // 默认 main.css
+    })
+  ]
+}
